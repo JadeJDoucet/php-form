717d03a47fef819cb051025c1be4328f46eac5fb
Jade Doucet <jadejdoucet@gmail.com>
2020-02-13 10:04:26 -0600

    added basic validation for form inputs

--- index.php
+++ index.php
@@ -12,29 +12,43 @@
         $ok = true;
 
         // Check for values to be filled
-        if (isset($_POST['name'])) {
+        if (!isset($_POST['name']) || $_POST['name'] === '') {
+            $ok = false;
+        } else {
             $name = $_POST['name'];
         }
-        if (isset($_POST['password'])) {
+        if (!isset($_POST['password']) || $_POST['password'] === '') {
+            $ok = false;
+        } else {
             $password = $_POST['password'];
         }
-        if (isset($_POST['gender'])) {
+        if (!isset($_POST['gender']) || $_POST['gender'] === '') {
+            $ok = false;
+        } else {
             $gender = $_POST['gender'];
         }
-        if (isset($_POST['color'])) {
+        if (!isset($_POST['color']) || $_POST['color'] === '') {
+            $ok = false;
+        } else {
             $color = $_POST['color'];
         }
-        if (isset($_POST['languages'])) {
+        if (!isset($_POST['languages']) || !is_array($_POST['languages']) || count($_POST['languages']) === 0) {
+            $ok = false;
+        } else {
             $languages = $_POST['languages'];
         }
-        if (isset($_POST['comments'])) {
+        if (!isset($_POST['comments']) || $_POST['comments' === '']) {
+            $ok = false;
+        } else {
             $comments = $_POST['comments'];
         }
-        if (isset($_POST['tc'])) {
+        if (!isset($_POST['tc']) || $_POST['tc'] === '') {
+            $ok = false;
+        } else {
             $tc = $_POST['tc'];
         }
 
-        // Output
+        // Output if all fields are set
         if ($ok) {
             printf('Username: %s
             <br>Password: %s
